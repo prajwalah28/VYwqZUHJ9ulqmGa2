51186b439b77d27f7fd59c90002e23b9a89a9f1f
prajwaldeveloper <prajwalhelunde@gmail.com>
2023-12-03 01:09:58 +0530

    Your commit message here

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -4,15 +4,15 @@ export const About = () => {
   return (
     <div>
       <h1>Candidate Details</h1>
-      <p>
-        <h2>First Name:</h2> <p>Prajwal </p>
-      </p>
-      <p>
-        <h2>Highest Degree Passout Year:</h2> <p>2024</p> <br></br><p>I am able to join internship from 15 december in my 8th sem</p>
-      </p>
-      <p>
-        <h2>Technologies/Languages:</h2> <p>Web development : HTML,CSS,ReactJS,PHP <br></br> Object oreinted programming : JAVA <br></br> DATABASE : MYSQL ,MongoDB</p>
-      </p>
+      
+        <b>First Name:</b> <p>Prajwal </p>
+      
+      
+        <b>Highest Degree Passout Year:</b> <p>2024</p> <br></br><p>I am able to join internship from 15 december in my 8th sem</p>
+      
+      
+        <b>Technologies/Languages:</b> <p>Web development : HTML,CSS,ReactJS,PHP <br></br> Object oreinted programming : JAVA <br></br> DATABASE : MYSQL ,MongoDB</p>
+      
       
     </div>
     
